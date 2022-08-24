3eb6c10f87a779de655a86df818aa447b08e983b
Nelson C <n.devpro@protonmail.com>
2022-08-24 11:24:58 -0400

    navbar navigation

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -28,7 +28,7 @@ const Navbar = () => {
             <button
               type="button"
               onClick={handleWriteClick}
-              className="font-text p-1 bg-mainColor rounded-md text-white tracking-wide shadow-md cursor-pointer flex items-center"
+              className="font-text p-1 bg-mainColor rounded-md text-white tracking-wide shadow-md cursor-pointer flex items-center transition duration-200 hover:scale-105"
             >
               Anotar
             </button>
