3e0ebe3bc1e0c1aa529ffbea8b0ee9194748303e
Nelson C <n.devpro@protonmail.com>
2022-08-24 11:24:19 -0400

    navbar navigation

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,7 +1,16 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { NavLink, useLocation, useNavigate } from 'react-router-dom';
+
+import { ModalContext } from '../../contexts/ModalContext';
 
 const Navbar = () => {
+  const { showModal, setShowModal } = useContext(ModalContext);
+  const navigate = useNavigate();
+  const location = useLocation();
+  const handleWriteClick = () => {
+    return location.pathname !== '/' ? navigate('/') : setShowModal(!showModal);
+  };
+
   return (
     <header className="w-full h-16 bg-white z-50 col-span-12 shadow-md fixed">
       <nav className="flex items-center justify-between px-4 h-full">
@@ -10,18 +19,19 @@ const Navbar = () => {
         </NavLink>
 
         <ul className="flex gap-5">
-          <li>
+          <li className="flex items-center">
             <NavLink to="/sobre" className="font-text cursor-pointer">
               Sobre
             </NavLink>
           </li>
-          <li>
-            <NavLink
-              to="/"
-              className="font-text p-2 bg-mainColor rounded-md text-white tracking-wide shadow-md cursor-pointer"
+          <li className="flex items-center">
+            <button
+              type="button"
+              onClick={handleWriteClick}
+              className="font-text p-1 bg-mainColor rounded-md text-white tracking-wide shadow-md cursor-pointer flex items-center"
             >
               Anotar
-            </NavLink>
+            </button>
           </li>
         </ul>
       </nav>
